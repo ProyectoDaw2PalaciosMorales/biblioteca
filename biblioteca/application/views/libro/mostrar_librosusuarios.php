0b04a896ba2d9cc24bd7bc552ea936a23bdf240e
sonic <sonic@DESKTOP-AFBG96S>
2021-05-05 19:28:00 +0200

    05/05/2021

--- biblioteca/application/views/libro/mostrar_librosusuarios.php
+++ biblioteca/application/views/libro/mostrar_librosusuarios.php
@@ -3,8 +3,50 @@
 
 	<!--  hacer cards -->
 		
+  <?php foreach ($libros as $libro): ?>
+<div class="card mb-3 mx-auto" style="width:80%">
+  <div class="row ">
+    <div class="col-md-3 offset-md-3 mx-auto">
+      <?php if ($libro ->foto!=null):?>
+
+   <img class="card-img mx-auto" src="<?=base_url()?>assets/fotoslibros/<?=$libro -> titulo;?>.<?=$libro -> foto;?>"
+					  style="width:50%">
+				<?php  else:?>
+	<img class="card-img mx-auto" src="<?=base_url()?>assets/fotoslibros/nodisponible.jpg"
+					  style="width:50%">
+			
+					 
+			<?php endif;?>	  
+
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title"><?=$libro->titulo?></h5>
+       <?php if ($libro ->descricion!=null):?>
+      <p class="card-text"><?=$libro->descricion?></p>
+      	<?php  else:?>
+      	 <p class="card-text">no hay descricion disponible </p>
+      	<?php endif;?>	
+      	 <form action="<?=base_url()?>libro/Libros/mostrarlibrosampliacion" method="post">
+				<input type="hidden" name="id" value="<?=$libro->id?>">
+				
+				  <button class="btn btn-primary stretched-link" onclick="submit()">
+					Ver mas
+						
+				 </button>				
+				
+			</form>    	     
+        
+      </div>
+    </div>
+  </div>
+</div>
+ 			
+		<?php endforeach;?>
+ 
+ 
   
-<div class="row row-cols-1 row-cols-md-3 g-4">
+<!--  <div class="row row-cols-1 row-cols-md-3 g-4">
 <?php foreach ($libros as $libro): ?>
 		
 
@@ -51,7 +93,7 @@
 		<?php endforeach;?>
 
 </div>
-    
+   --> 
           <?php 
           $url=base_url()."libro/Libros/panel_administrativo";
          // header('refresh:30; url='.$url);?>
