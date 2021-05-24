925ef727c0a9e64747672518ff117f687d3321e6
sonic <sonic@DESKTOP-AFBG96S>
2021-05-24 18:27:52 +0200

    tablas 24/05/2021

--- biblioteca/application/views/_templates/nav.php
+++ biblioteca/application/views/_templates/nav.php
@@ -3,7 +3,7 @@
 
  <div class="navbar-header  ">
  
-      <a class="navbar-brand"  href='<?=base_url()?>'>TU BIBLIOTECA</a>
+      <a class="navbar-brand"  style="margin-top:-50px" href='<?=base_url()?>'>TU BIBLIOTECA</a>
     </div>
   <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
@@ -12,12 +12,12 @@
     <ul class="navbar-nav "			>
    		
        
-          <li class="nav-item" > <a class="text-white nav-link" style="font-size: 15px;margin-top:25px;margin-left:10px;" href='<?=base_url()?>autor/Autores/autor'>Elija un autor para ver sus obras</a></li>
-          <li class="nav-item"><a class="text-white nav-link"  style="font-size: 15px;margin-top:25px;margin-left:10px;"href='<?=base_url()?>libro/Libros/genero'>Tus genereros favoritos</a></li>
+          <li class="nav-item" > <a class="text-white nav-link" style="font-size: 15px;margin-top:25px;margin-left:30px;" href='<?=base_url()?>autor/Autores/autor'>Elija su autor </a></li>
+          <li class="nav-item"><a class="text-white nav-link"  style="font-size: 15px;margin-top:25px;margin-left:30px;"href='<?=base_url()?>libro/Libros/genero'>Elija su genereros favoritos</a></li>
          
 
                <?php if (isset( $_SESSION['nombre'])!=null):?>    
-                <li class="nav-item"><a class="text-white nav-link" style="font-size: 15px;margin-top:25px ;margin-left:10px;" href='<?=base_url()?>usuario/Usuarios/Bienvenidos_u'>Menu principal</a></li>
+                <li class="nav-item"><a class="text-white nav-link" style="font-size: 15px;margin-top:25px ;margin-left:30px;" href='<?=base_url()?>usuario/Usuarios/Bienvenidos_u'>Menu principal</a></li>
                  <?php  else:?>
                   <?php endif;?>
        
@@ -35,16 +35,16 @@
          <?php if (  !isset( $_SESSION['nombre'])):?>            
         
         <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-       <img class="avatar" src="<?=base_url()?>assets/fotosperfil/noimagen.jpg"style="font-size: 15px;margin-left:130px;margin-top:25px;"> </a>
+       <img class="avatar" src="<?=base_url()?>assets/fotosperfil/noimagen.jpg"style="font-size: 15px;margin-left:110px;margin-top:25px;"> </a>
        
          <?php  else:?>
            <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
             <?php 
             $sustitutuirespaciosblancos = str_replace(" ","_",$_SESSION['nombre'])?>
           <?php if (is_file("assets/fotosperfil/usuario-". $sustitutuirespaciosblancos.".png" )):?>
-        <img class="avatar" src="<?=base_url()?>assets/fotosperfil/usuario-<?= $sustitutuirespaciosblancos?>.png"style="font-size: 25px;margin-left:130px;margin-top:25px;">
+        <img class="avatar" src="<?=base_url()?>assets/fotosperfil/usuario-<?= $sustitutuirespaciosblancos?>.png"style="font-size: 25px;margin-left:110px;margin-top:25px;">
          <?php  else:?>
-         <img class="avatar" src="<?=base_url()?>assets/fotosperfil/noimagen.jpg"style="font-size: 15px;margin-left:130px;margin-top:25px;">
+         <img class="avatar" src="<?=base_url()?>assets/fotosperfil/noimagen.jpg"style="font-size: 15px;margin-left:110px;margin-top:25px;">
                     <?php endif;?>
       </a>
         
@@ -52,16 +52,16 @@
      
         <ul class="dropdown-menu bg-dark ">
         <?php if (  !isset( $_SESSION['nombre'])):?>  
-         <li> <a class="text-white nav-link" href='<?=base_url()?>usuario/Usuarios/accesoget'>login</a></li>
+         <li> <a   style="margin-left:12px;" class="text-white nav-link" href='<?=base_url()?>usuario/Usuarios/accesoget'>Iniciar sesion</a></li>
          <?php  else:?>
-          <li><form  name="formulario1" action="<?=base_url()?>usuario/Usuarios/perfil_usuario" method="post">
+          <li><form style="margin-left:12px;" name="formulario1" action="<?=base_url()?>usuario/Usuarios/perfil_usuario" method="post">
 
 					<input type="hidden" name="id" value="<?=$_SESSION['idusuario']?>">
 								          
 					<a class="text-white text-decoration-none" href='javascript:enviar_formulario()'>Perfil</a>
 				  </form></li>
 				  
-          <li> <a class="text-white  text-decoration-none" href='<?=base_url()?>usuario/Usuarios/cerrar'>Cerrar sesion</a></li>
+          <li> <a  style="margin-left:12px;" class="text-white  text-decoration-none" href='<?=base_url()?>usuario/Usuarios/cerrar'>Cerrar sesion</a></li>
          <?php endif;?>
         </ul>
       </li>
